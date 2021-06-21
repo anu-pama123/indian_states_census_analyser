dd92e279f86b0108682cc473afc18b7c5e20449c
anupama cv <anu.anupamacv@gmail.com>
2021-06-21 23:37:12 +0530

    Added: new method to overcome dry principle violation

--- src/main/java/com/bridgelabz/CensusAnalyser.java
+++ src/main/java/com/bridgelabz/CensusAnalyser.java
@@ -11,6 +11,12 @@ import java.util.Iterator;
 import java.util.stream.StreamSupport;
 
 public class CensusAnalyser {
+    private <E> int recordsCounter(Iterator<E> iterator) {
+        Iterable<E> censusCSVIterable = () -> iterator;
+        int numOfEntries = (int) StreamSupport.stream(censusCSVIterable.spliterator(), false).count();
+        return numOfEntries;
+    }
+
     public int loadCensusData(String csvFilePath) throws CensusAnalyserException {
         try {
             Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
@@ -19,9 +25,7 @@ public class CensusAnalyser {
             csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
             CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.withSeparator(',').build();
             Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();
-            Iterable<IndiaCensusCSV> censusCSVIterable = () -> censusCSVIterator;
-            int numOfEntries = (int) StreamSupport.stream(censusCSVIterable.spliterator(), false).count();
-            return numOfEntries;
+            return recordsCounter(censusCSVIterator);
         } catch (IOException e) {
             throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
         }
@@ -38,11 +42,12 @@ public class CensusAnalyser {
             csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
             CsvToBean<StateCodeCSV> csvToBean = csvToBeanBuilder.withSeparator(',').build();
             Iterator<StateCodeCSV> censusCSVIterator = csvToBean.iterator();
-            Iterable<StateCodeCSV> censusCSVIterable = () -> censusCSVIterator;
-            int numOfEntries = (int) StreamSupport.stream(censusCSVIterable.spliterator(), false).count();
-            return numOfEntries;
+            return recordsCounter(censusCSVIterator);
         } catch (IOException e) {
             throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        }
+        catch (RuntimeException e) {
+            throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.WRONG_DELIMITER_OR_HEADER);
         }
     }
 }
